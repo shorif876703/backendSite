98a3f724e53b3662d79530186cb85b7574612e39
shorif3378 <shorif876703@gmail.com>
2025-10-03 21:53:47 +0600

    url changed

--- index.js
+++ index.js
@@ -11,7 +11,12 @@ import connectToDatabase from "./db/db.js";
 
 connectToDatabase();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://employe-frontend.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.static("public/uploads"));
 app.use("/api/auth", authRouter);
